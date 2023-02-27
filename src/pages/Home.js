0c21eb295ad521f3a82da32755140e93dda238eb
RudrangsuChoudhury <choudhuryrudrangsu@gmail.com>
2023-02-28 00:11:28 +0530

    upd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ import SpecialProduct from '../components/SpecialProduct';
 import FamousProduct from '../components/FamousProduct';
 import ProductCard from '../components/ProductCard';
 import background from '../images/background1.png';
-import darkbackground from '../images/darkbackground.png';
+import darkbackground from '../images/darkBackground.png';
 import Shop from '../images/background-shop.jpg';
 import PS5 from '../images/PS5.jpg';
 import mac from "../images/13-inch-macbook-pro-m2-mock-feature-2.jpg"
