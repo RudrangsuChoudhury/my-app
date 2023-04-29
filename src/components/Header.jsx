e72d3ba7c666b6dfe1fd2142782456b72c998813
RudrangsuChoudhury <choudhuryrudrangsu@gmail.com>
2023-04-30 01:25:36 +0530

    ds

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -187,7 +187,7 @@ display={scrollDirection === "down" && (location.pathname === '/' || location.pa
               fontWeight={1000}
               fontFamily="Calligraffitti"
             >
-              <Link>The Student's Marketplace</Link>
+              <Link>The Student's Marketplacecdc</Link>
             </Text>
             <Text
               fontSize={['0.7em','0.7em',"1xl"]}
